5acb4a1529dee60e534469565bea5c80e1570c15
Vanitas-del <rrai97381@gmail.com>
2024-07-08 22:25:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ let lights = document.querySelectorAll('.light');
 let reactionTimeElement = document.getElementById('time');
 let startTime;
 let endTime;
+let timerRunning = false;
 
 document.addEventListener('DOMContentLoaded', function() {
     let intervalId = setInterval(function() {
-        let randomTime = Math.random() * 3000 + 1000; // random time between 1-4 seconds
-        setTimeout(function() {
-            lights.forEach(function(light) {
-                light.classList.add('on');
-            });
-            startTime = new Date().getTime();
-        }, randomTime);
+        if (!timerRunning) {
+            let randomTime = Math.random() * 3000 + 1000; // random time between 1-4 seconds
+            setTimeout(function() {
+                lights.forEach(function(light) {
+                    light.classList.add('on');
+                });
+                startTime = new Date().getTime();
+                timerRunning = true;
+            }, randomTime);
+        }
     }, 5000); // repeat every 5 seconds
 });
 
 document.addEventListener('click', function() {
-    endTime = new Date().getTime();
-    let reactionTime = endTime - startTime;
-    reactionTimeElement.textContent = reactionTime + ' ms';
-    lights.forEach(function(light) {
-        light.classList.remove('on');
-    });
+    if (timerRunning) {
+        endTime = new Date().getTime();
+        let reactionTime = endTime - startTime;
+        reactionTimeElement.textContent = reactionTime + 's';
+        lights.forEach(function(light) {
+            light.classList.remove('on');
+        });
+        timerRunning = false;
+    }
 });

55b7dfda20032441305ae9956666490fdf46d493
Vanitas-del <rrai97381@gmail.com>
2024-07-08 22:53:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ let timerRunning = false;
 let fastestTime = localStorage.getItem('fastestTime');
 
 if (fastestTime) {
-    fastestTimeElement.textContent = fastestTime + 's';
+  fastestTimeElement.textContent = fastestTime + 's';
 } else {
-    fastestTimeElement.textContent = '-';
+  fastestTimeElement.textContent = '-';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    let intervalId = setInterval(function() {
-        if (!timerRunning) {
-            let randomTime = Math.random() * 3000 + 1000; // random time between 1-4 seconds
-            setTimeout(function() {
-                lights.forEach(function(light) {
-                    light.classList.add('on');
-                });
-                startTime = new Date().getTime();
-                timerRunning = true;
-            }, randomTime);
-        }
-    }, 5000); // repeat every 5 seconds
+  let intervalId = setInterval(function() {
+    if (!timerRunning) {
+      let randomTime = Math.random() * 3000 + 1000; // random time between 1-4 seconds
+      setTimeout(function() {
+        lights.forEach(function(light) {
+          light.classList.add('on');
+        });
+        startTime = new Date().getTime();
+        timerRunning = true;
+      }, randomTime);
+    }
+  }, 5000); // repeat every 5 seconds
 });
 
 document.addEventListener('click', function() {
-    if (timerRunning) {
-        endTime = new Date().getTime();
-        let reactionTime = endTime - startTime;
-        reactionTimeElement.textContent = reactionTime + 'ms';
-        lights.forEach(function(light) {
-            light.classList.remove('on');
-        });
-        timerRunning = false;
+  if (timerRunning) {
+    endTime = new Date().getTime();
+    let reactionTime = endTime - startTime;
+    reactionTimeElement.textContent = reactionTime + 's';
+    lights.forEach(function(light) {
+      light.classList.remove('on');
+    });
+    timerRunning = false;
 
-        if (!fastestTime || reactionTime < fastestTime) {
-            fastestTime = reactionTime;
-            fastestTimeElement.textContent = fastestTime + 'ms';
-            localStorage.setItem('fastestTime', fastestTime);
-        }
+    if (!fastestTime || reactionTime < fastestTime) {
+      fastestTime = reactionTime;
+      fastestTimeElement.textContent = fastestTime + 's';
+      localStorage.setItem('fastestTime', fastestTime);
     }
+  }
 });
